658d2e2c909b6221cb523b46c455306b097879b3
clarebryar <128009509+clarebryar@users.noreply.github.com>
2023-05-08 20:23:51 -0500

    changes on live share

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+// Maps API key
+// AIzaSyDOMF9Qvb_zajNGVx1sVSlEEZwEnANziH0
+
+// https://www.google.com/maps/search/?api=1&parameters
+
+// Directions
+
+var mapDiv = document.querySelector("#tempmapdiv")
+var googleDirections =  https://www.google.com/maps/dir/?api=1&origin={STARTINGPOINT}&destination={ENDPOINT}&travelmode=driving&map_action=map
+
+	
+// DirectionsService()
+// origin - defines the starting point from which to display directions 
+//destination - endpoint of the directions 
+
+function displayMap () {
+    var googleDirections =  https://www.google.com/maps/dir/?api=1&origin=Chicago&destination=Milwaukee&travelmode=driving&map_action=map
+}
