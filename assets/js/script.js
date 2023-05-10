84fbe17cf4bd745361d9916394f331c9687d072c
elenaf007 <96072892+elenaf007@users.noreply.github.com>
2023-05-10 18:44:20 -0500

    adding map direction

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
-var origin=document.getElementById('start-point').value;
-var destination=document.getElementById('destination').value;
+//var origin=document.getElementById('start-point').value;
+//var destination=document.getElementById('destination').value;
 var searchBtn=document.getElementById('start-btn')
+var googleApi='AIzaSyDOMF9Qvb_zajNGVx1sVSlEEZwEnANziH0'
 //var mapDiv=document.createElement('div')
 // Maps API key
 // AIzaSyDOMF9Qvb_zajNGVx1sVSlEEZwEnANziH0
@@ -10,10 +11,10 @@ var searchBtn=document.getElementById('start-btn')
 // Directions
 
 var mapDiv = document.querySelector("#tempmapdiv")
-var googleDirections =  'https://www.google.com/maps/dir/?api=1&origin={origin}&destination={destination}&travelmode=driving&map_action=map'
+var googleDirections =  'https://www.google.com/maps/dir/?api=1&origin={start}&destination={end}&travelmode=driving&map_action=map'
 
 	
-// DirectionsService()
+//var DirectionsService;
 // origin - defines the starting point from which to display directions 
 //destination - endpoint of the directions 
 
@@ -22,9 +23,11 @@ var googleDirections =  'https://www.google.com/maps/dir/?api=1&origin={origin}&
 
 //     const {DirectionsService} = await google.maps.importLibrary("routes")
 // Initialize and add the map
-let map;
-
+/*
+var map;
 async function initMap() {
+  var start=document.getElementById('start-point').value;
+  var end=document.getElementById('destination').value;
   // The location of Uluru
   const position = { lat: 41.882, lng: -87.623 };
   // Request needed libraries.
@@ -46,30 +49,94 @@ async function initMap() {
     map: map,
     position: position,
     title: "Uluru",
-    
+    //calculateAndDisplayRoute
   });
-}
 
-initMap();
+ calculateAndDisplayRoute()
+}*/
+
+//initMap();
 /*
-var direction = new google.maps.DirectionsService()
-directionsService.route(directionsService.route()
-{
-    origin: "Chicago"
-    destination: "New York"
-    travelMode: "DRIVING"
-},
-(response, status) => {
-    console.log(response)
-    console.log(status)
+function calculateAndDisplayRoute(directionsService){
+  var direction =new google.maps.DirectionsService()
+  console.log(direction);
+  var start=document.getElementById('start-point').value;
+  var end=document.getElementById('destination').value;
+  console.log(start, end);
+  var request={
+    origin: start,
+    destination: end,
+    travelMode: 'DRIVING',
+  };
+  console.log(request);
+  var direction =new google.maps.DirectionsService()
+  console.log(direction);
+  directionsService.route(request,function(response,status) {
+        
+    if (status==google.maps.DirectionsStatus.OK) {
+      var direction =new google.maps.DirectionsService()
+      directionsDisplay.setDirections(response);
+      console.log(direction);
+    }
+  });
+};
+ 
+function calculateAndDisplayRoute(directionsService, directionsRenderer) {
+  var start=document.getElementById('start-point').value;
+  var end=document.getElementById('destination').value;
+  console.log(start, end);
+  var request={
+    origin: start,
+    destination: end,
+    travelMode: 'DRIVING',
+  };
+  directionsService
+    .route(request)
+    .then((response) => {
+      directionsRenderer.setDirections(response);
+    })
+    .catch((e) => window.alert("Directions request failed due to " + status));
 }
-);*/
+
+window.initMap = initMap;
+searchBtn.addEventListener('click', initMap); */
+
+var map;
+var waypoints;
+function initMap() {
+  var mapLayer = document.getElementById("tempmapdiv"); 
+  var centerCoordinates = new google.maps.LatLng(41.882,  -87.623);
+  var defaultOptions = { center: centerCoordinates, zoom: 8 }
+  map = new google.maps.Map(mapLayer, defaultOptions);
+	
+  var directionsService = new google.maps.DirectionsService;
+  var directionsDisplay = new google.maps.DirectionsRenderer;
+  directionsDisplay.setMap(map);
+ 
+  searchBtn.addEventListener("click",function() {
+    var start=document.getElementById('start-point').value;
+  var end=document.getElementById('destination').value;
+     drawPath(directionsService, directionsDisplay,start,end);
+              
+  });
+            
+}
+
+function drawPath(directionsService, directionsDisplay,start,end) {
+ directionsService.route({
+  origin: start,
+  destination: end,
+  optimizeWaypoints: true,
+  travelMode: 'DRIVING',
+     }, function(response, status) {
+                if (status === 'OK') {
+                directionsDisplay.setDirections(response);
+                } else {
+                window.alert('Problem in showing direction due to ' + status);
+                }
+            });
+      }
 
 
 
-
-
-
-
-    
     
