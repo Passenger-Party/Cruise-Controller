555d99c6fb924718d1f6baf3e323fc9567d95028
northgoddard <129123028+northgoddard@users.noreply.github.com>
2023-05-11 22:15:54 -0700

    weather man north

--- assets/js/mapsscript.js
+++ assets/js/mapsscript.js
@@ -142,4 +142,38 @@ function drawPath(directionsService, directionsDisplay, start, end) {
   );
 }
 
+function GetInfo(){
+  var destination = localStorage.getItem("Destination");
+
+  fetch('https://api.openweathermap.org/data/2.5/forecast?&units=imperial&q='+destination+'&appid=88f279a121e9f3c2da0f526adc7c151d')
+  .then(Response => Response.json())
+  .then(data => {
+    console.log(data);
+      for(i = 0; i<5; i++){
+          document.getElementById("day" + (i+1) + "Min").innerHTML = "Low: " + Math.round(data.list[i].main.temp_min).toFixed(1)+ "0°";
+      }
+      for(i = 0; i<5; i++){
+          document.getElementById("day" + (i+1) + "Max").innerHTML = "High: " + Math.round(data.list[i].main.temp_max).toFixed(2)+ "°";
+      }
+      for(i = 0; i<5; i++){
+          document.getElementById("img" + (i+1)).src = "http://openweathermap.org/img/wn/"+data.list[i].weather[0].icon+".png";
+      }
+  })
+}
+
+var d = new Date();
+var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+function CheckDay(day){
+  if(day + d.getDay() > 6){
+      return day + d.getDay() - 7;
+  }
+  else{
+      return day + d.getDay();
+  }
+}
+for(i = 0; i<5; i++){
+  document.getElementById("day" + (i+1)).innerHTML = weekday[CheckDay(i)];
+}
+
 
